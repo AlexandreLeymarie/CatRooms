464d8d9d6332e7e4bc5a3d2ccb2633ed7a3661a1
Alexandre LEYMARIE <alexandre.j.leymarie@gmail.com>
2025-09-07 23:33:02 +0200

    basic mouse movement

--- main.js
+++ main.js
@@ -5,30 +5,49 @@ cv.width = 854;
 cv.height = 480;
 
 
-rot=(v,a)=>{let c=Math.cos(a),s=Math.sin(a);return vec(v.x*c-v.y*s,v.x*s+v.y*c)}
+const rotate=(v,a)=>{let c=Math.cos(a),s=Math.sin(a);return vec(v.x*c-v.y*s,v.x*s+v.y*c)}
+const stc=(p,c)=>rotate(p.sub(c.pos),-c.rot).mul(c.zoom).add(vec(cv.width/2,cv.height/2))
+const cts=(p,c)=>rotate(p.sub(vec(cv.width/2,cv.height/2)).mul(1/c.zoom),c.rot).add(c.pos)
 
-stc=(p,c)=>rot(p.sub(c.pos),-c.a).mul(c.zoom).add(vec(cv.width/2,cv.height/2))
-cts=(p,c)=>rot(p.sub(vec(cv.width/2,cv.height/2)).mul(1/c.zoom),c.a).add(c.pos)
 
+const keyList = [];
+window.addEventListener('keydown',function(event){
+    keyList[event.code] = true;
+    if(["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"].indexOf(event.code) > -1) {
+        event.preventDefault();
+    }
+});
+window.addEventListener('keyup',function(event){
+    keyList[event.code] = false;
+});
 
 function Mouse(world, pos){
     this.world = world;
     this.pos = pos;
     this.vel = vec();
     this.rot = 0;
+    this.rotVel = 0;
     this.radius = 0.4;
 }
 
 Mouse.prototype.update = function(dt){
-    this.rot += dt;
+    let velTarget = vec(Math.cos(this.rot), Math.sin(this.rot)).mul((keyList["ArrowUp"]?10:0)-(keyList["ArrowDown"]?5:0));
+    if(keyList["ArrowUp"]){
+        velTarget = vec(Math.cos(this.rot), Math.sin(this.rot)).mul(10);
+    }
+    let rotVelTarget = ((keyList["ArrowRight"]?1:0)-(keyList["ArrowLeft"]?1:0))*.1;
+    this.rotVel += (rotVelTarget-this.rotVel)*(1-Math.pow(1-0.9, dt/0.2))
+    this.rot += this.rotVel;
+    this.vel = this.vel.add(velTarget.sub(this.vel).mul(1-Math.pow(1-0.9, dt/0.2)));
+    this.pos = this.pos.add(this.vel.mul(dt));
 }
 
 Mouse.prototype.draw = function(cam){
     ctx.fillStyle = "rgb(125, 92, 54)";
     ctx.beginPath();
     let cvPos = stc(this.pos, cam);
-    ctx.arc(cvPos.x, cvPos.y, this.radius*cam.zoom, this.rot-cam.a+Math.PI/2, this.rot-cam.a+3*Math.PI/2);
-    let r = this.radius*1.5
+    ctx.arc(cvPos.x, cvPos.y, this.radius*cam.zoom, this.rot-cam.rot+Math.PI/2, this.rot-cam.rot+3*Math.PI/2);
+    let r = this.radius*1.2;
     let noseP = stc(this.pos.add(vec(r*Math.cos(this.rot), r*Math.sin(this.rot))), cam);
     ctx.lineTo(noseP.x, noseP.y);
     ctx.closePath();
@@ -39,7 +58,7 @@ Mouse.prototype.draw = function(cam){
 function World(){
     this.mouse = new Mouse(this, vec(-1));
     this.objects = [this.mouse];
-    this.cam = {pos: vec(), a: 0, zoom: 40};
+    this.cam = {pos: vec(), rot: 0, zoom: 40};
     let gridStr = 
 `
 aaa aaaa
@@ -65,8 +84,8 @@ aaaaaaaa
 }
 
 World.prototype.update = function(dt){
-    this.cam.a = this.mouse.rot+Math.PI/2;
-    this.cam.pos = this.mouse.pos.copy();
+    this.cam.rot += ((this.mouse.rot+Math.PI/2)-this.cam.rot)*(1-Math.pow(1-0.95, dt));
+    this.cam.pos = this.cam.pos.add(this.mouse.pos.sub(this.cam.pos).mul(1-Math.pow(1-0.95, dt/.2)));
     for(object of this.objects){
         if(object.update !== undefined){
             object.update(dt);
