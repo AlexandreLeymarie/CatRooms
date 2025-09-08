ecebf717549f9c95c06aed248f4123d02e1d7d86
Alexandre LEYMARIE <alexandre.j.leymarie@gmail.com>
2025-09-08 23:50:42 +0200

    Add start of cat

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
+// npx terser main.js -o output.js -c -m --toplevel
 const cv = document.getElementById("cv");
 const ctx = cv.getContext("2d");
 
 cv.width = 854;
 cv.height = 480;
 
-
 const rotate=(v,a)=>{let c=Math.cos(a),s=Math.sin(a);return vec(v.x*c-v.y*s,v.x*s+v.y*c)}
-const stc=(p,c)=>rotate(p.sub(c.pos),-c.rot).mul(c.zoom).add(vec(cv.width/2,cv.height/2))
-const cts=(p,c)=>rotate(p.sub(vec(cv.width/2,cv.height/2)).mul(1/c.zoom),c.rot).add(c.pos)
+const spaceToCanvas=(p,cam)=>rotate(p.sub(cam.pos),-cam.rot).mul(cam.zoom).add(vec(cv.width/2,cv.height/2))
+const canvasToSpace=(p,cam)=>rotate(p.sub(vec(cv.width/2,cv.height/2)).mul(1/cam.zoom),cam.rot).add(cam.pos)
 
 
 const keyList = [];
@@ -74,19 +74,81 @@ Mouse.prototype.resolveCollisions = function(){
 Mouse.prototype.draw = function(cam){
     ctx.fillStyle = "rgb(125, 92, 54)";
     ctx.beginPath();
-    let cvPos = stc(this.pos, cam);
+    let cvPos = spaceToCanvas(this.pos, cam);
     ctx.arc(cvPos.x, cvPos.y, this.radius*cam.zoom, this.rot-cam.rot+Math.PI/2, this.rot-cam.rot+3*Math.PI/2);
     let r = this.radius*1.2;
-    let noseP = stc(this.pos.add(vec(r*Math.cos(this.rot), r*Math.sin(this.rot))), cam);
+    let noseP = spaceToCanvas(this.pos.add(vec(r*Math.cos(this.rot), r*Math.sin(this.rot))), cam);
     ctx.lineTo(noseP.x, noseP.y);
     ctx.closePath();
     ctx.fill();
 }
 
+function Cat(world, pos){
+    this.world = world;
+    this.pos = pos;
+    this.vel = vec(0);
+    let d = 2.5;
+    this.head = this.pos.add(vec(0,d));
+    this.pawsOffset = [vec(-d, -d),vec(d, -d),vec(d,d),vec(-d,d)];
+    this.paws = [];
+    this.pawsTargets = [];
+    for(let i = 0; i < 4; i++){
+        this.paws[i] = this.pos.add(this.pawsOffset[i]);
+        this.pawsTargets[i] = this.paws[i];
+    }
+    this.headR = 1.2;
+    this.pawR = 0.8;
+    this.bodyR = 1.6;
+    this.legL = 3;
+}
+
+Cat.prototype.update = function(dt){
+    let d = this.world.mouse.pos.sub(this.pos);
+    let velTarget = d.normalize().mul(5);
+    this.vel = this.vel.add(velTarget.sub(this.vel).mul(1-Math.pow(1-0.9, dt/0.5)));
+    this.pos = this.pos.add(this.vel.mul(dt));
+
+    let headTarget = this.pos.add(this.vel.normalize().mul(4))
+    this.head = this.head.add(headTarget.sub(this.head).mul(1-Math.pow(1-0.8, dt/0.5)));
+
+    for(let i = 0; i < 4; i++){
+        let target = this.pos.add(rotate(this.pawsOffset[i], Math.atan2(-this.vel.y, this.vel.x)));
+        if(this.paws[i].sub(target).length() > this.legL){
+            this.pawsTargets[i] = d.length() < this.legL*2 ? this.world.mouse.pos : target;
+        }
+        this.paws[i] = this.paws[i].add(this.pawsTargets[i].sub(this.paws[i]).mul(1-Math.pow(1-0.8, dt/0.1)));
+    }
+}
+
+Cat.prototype.draw = function(cam){
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    let p = spaceToCanvas(this.head, cam);
+    ctx.arc(p.x, p.y, this.headR*cam.zoom, 0, Math.PI*2);
+    ctx.fill();
+
+    ctx.beginPath();
+    p = spaceToCanvas(this.pos, cam);
+    ctx.arc(p.x, p.y, this.bodyR*cam.zoom, 0, Math.PI*2);
+    ctx.fill();
+
+    let i = 0;
+    for(let pa of this.paws){
+        if(true){
+            ctx.beginPath();
+            p = spaceToCanvas(pa, cam);
+            ctx.arc(p.x, p.y, this.pawR*cam.zoom, 0, Math.PI*2);
+            ctx.fill();
+        }
+
+        i++;
+    }
+}
 
 function World(){
     this.mouse = new Mouse(this, vec(-1));
-    this.objects = [this.mouse];
+    this.cat = new Cat(this, vec(-5));
+    this.objects = [this.mouse, this.cat];
     this.cam = {pos: vec(), rot: 0, zoom: 40};
     let gridStr = 
 `
@@ -143,7 +205,7 @@ World.prototype.draw = function(){
                 let corners = [[0,0], [0,1], [1,1], [1,0]];
                 for(let ind = 0; ind < corners.length; ind++){
                     let cornerPos = vec(j, i).add(vec(corners[ind][0], corners[ind][1]))
-                    let cvc = stc(cornerPos, this.cam);
+                    let cvc = spaceToCanvas(cornerPos, this.cam);
                     if(ind == 0){
                         ctx.moveTo(cvc.x, cvc.y);
                     } else {
